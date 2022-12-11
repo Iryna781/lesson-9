0401b23b2a16b66294591687ff9d0516f6d2b443
Iryna <mulatka781@gmail.com>
2022-12-11 10:25:57 +0200

    name

---
+++ src/Content.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const Content = () => {
+    return (
+        // <React.Fragment>
+        <>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+                quaerat distinctio repellendus minus quibusdam et laudantium
+                inventore magnam eum exercitationem quidem consectetur nihil
+                dolores perferendis nam, unde porro numquam saepe.
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima
+                quasi illum ut voluptatibus, maiores nam harum vero enim dolorem
+                corrupti iusto odit accusamus, omnis fugit necessitatibus dolore
+                quam quidem dolorum.
+            </p>
+        </>
+        // </React.Fragment>
+    )
+}
+export default Content
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { Content } from './Content'
 // PascalCase
 // function App() {
 //     return (
@@ -32,34 +32,11 @@ const Title = ({ title, text = 'TEST' }: TitleProps) => {
     )
 }
 
-const Content = () => {
-    return (
-        // <React.Fragment>
-        <>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
-                quaerat distinctio repellendus minus quibusdam et laudantium
-                inventore magnam eum exercitationem quidem consectetur nihil
-                dolores perferendis nam, unde porro numquam saepe.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima
-                quasi illum ut voluptatibus, maiores nam harum vero enim dolorem
-                corrupti iusto odit accusamus, omnis fugit necessitatibus dolore
-                quam quidem dolorum.
-            </p>
-        </>
-        // </React.Fragment>
-    )
-}
 const App = () => {
     return (
         // <React.Fragment>
         <>
             <Title text="Hello" title="React" />
-            <Title text="Hi" title="About" />
-            <Title title="Bla-bla" />
-
             <Content />
         </>
         // </React.Fragment>
