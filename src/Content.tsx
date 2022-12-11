cd2bba972e48fd0224bae13d8f1c66414c57244d
Iryna <mulatka781@gmail.com>
2022-12-11 10:54:44 +0200

    tomato

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './Content.css'
 type Props = {
     p1: string
     p2: string
@@ -8,7 +8,7 @@ type Props = {
 const Content = ({ p1, p2 }: Props) => {
     return (
         <>
-            <p>{p1}</p>
+            <p className="tomato">{p1}</p>
             <p>{p2}</p>
         </>
     )
