72c850d79284426dc2f82de61633f901b03c9a06
Iryna <mulatka781@gmail.com>
2022-12-18 10:20:10 +0200

    Phone

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,14 @@
 import { Typography } from '@mui/material'
+import ProductsList from 'components/Products/ProductsList'
 
 type Props = {}
 const Home = (props: Props) => {
     return (
         <>
             <Typography variant="h4" align="center">
-                {' '}
-                List of Products{' '}
+                List of Products
             </Typography>
+            <ProductsList />
         </>
     )
 }
