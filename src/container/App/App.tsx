c93c61b44356fdf3e786eb991439ae62e5e36180
Iryna <mulatka781@gmail.com>
2023-01-08 11:03:53 +0200

    cart page

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -9,7 +9,10 @@ type ProductsInCart = {
 }
 
 const App = () => {
-    const [productsInCart, setProductsInCart] = useState<ProductsInCart>({})
+    const [productsInCart, setProductsInCart] = useState<ProductsInCart>({
+        1: 4,
+        2: 1,
+    })
 
     const addProductToCart = (id: number, count: number) => {
         setProductsInCart((prevState: ProductsInCart) => ({
@@ -22,7 +25,10 @@ const App = () => {
             <StyledEngineProvider injectFirst>
                 <CssBaseline />
                 <Header productsInCart={productsInCart} />
-                <Main addProductToCart={addProductToCart} />
+                <Main
+                    addProductToCart={addProductToCart}
+                    productsInCart={productsInCart}
+                />
             </StyledEngineProvider>
         </>
     )
--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -8,8 +8,11 @@ import { Route, Routes } from 'react-router-dom'
 
 type Props = {
     addProductToCart: (id: number, count: number) => void
+    productsInCart: {
+        [id: number]: number
+    }
 }
-const Main = ({ addProductToCart }: Props) => {
+const Main = ({ addProductToCart, productsInCart }: Props) => {
     return (
         <main
             style={{
@@ -25,7 +28,10 @@ const Main = ({ addProductToCart }: Props) => {
                     <Route path="about" element={<AboutPage />} />
                     <Route path="payment" element={<PaymentPage />} />
                     <Route path="shipping" element={<ShippingPage />} />
-                    <Route path="cart" element={<CartPage />} />
+                    <Route
+                        path="cart"
+                        element={<CartPage productsInCart={productsInCart} />}
+                    />
                 </Routes>
             </Container>
         </main>
